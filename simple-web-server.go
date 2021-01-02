6cddf7c5aafbe416e32e9b2538f26b542c0d352f
andrew-collett <andrew.collett@me.com>
2021-01-02 12:55:14 +0000

    Update simple-web-server.go
    Updated the response to add YAY

--- simple-web-server.go
+++ simple-web-server.go
@@ -6,7 +6,7 @@ import (
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "I am a Tolrendor Software GO application running inside Docker.")
+	fmt.Fprintf(w, "I am a Tolrendor Software GO application running inside Docker - YAY.")
 
 }
 
